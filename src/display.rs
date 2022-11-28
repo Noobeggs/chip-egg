c4021ddca9455c4c7cc96d8a4a70aa60cd91e8e4
Chiew Yan Wei <chiewyanwei@gmail.com>
2022-11-28 21:29:51 +0800

    Implement draw function for Display + Fix some errors.

--- src/display.rs
+++ src/display.rs
@@ -1,3 +1,54 @@
 pub struct Display {
-    display: [[u8; 64]; 32],
+    display: [[u8; 32]; 64],
+    width: u8,
+    height: u8,
+}
+
+impl Display {
+    pub fn new() -> Display {
+        Display {
+            display: [[0; 32]; 64],
+            width: 64,
+            height: 32,
+        }
+    }
+
+    pub fn draw(&mut self, sprite: &[u8], vx: u8, vy: u8) -> u8 {
+        let x = vx & (self.width-1);
+        let y = vy & (self.height-1);
+        let mut collision = 0;
+
+        // for (row, sprite_row) in sprite.iter().enumerate() {
+        //     if row + y as usize >= self.height as usize {
+        //         break;
+        //     }
+        //     for (col, sprite_pixel) in sprite_row.iter().enumerate() {
+        //         if (col + x) as usize >= self.width as usize {
+        //             break;
+        //         }
+        //         if sprite_pixel == 1 {
+        //             if collision == 0 && display[x][y] == 1 {
+        //                 collision = 1
+        //             }
+        //             display[x][y] ^= 1
+        //         }
+        //     }
+        // }
+
+        for (row, sprite_row) in sprite.iter().enumerate() {
+            if row + y as usize >= self.height as usize {
+                break;
+            }
+            for pixel in 0..8 {
+                if sprite_row & (0x80 >> pixel) != 0 {
+                    if collision == 0 && self.display[x as usize + pixel][y as usize + row] == 1 {
+                        collision = 1
+                    }
+                    self.display[x as usize + pixel][y as usize + row] ^= 1
+                }
+            }
+        }
+
+        collision
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -29,11 +29,11 @@ impl Chip8 {
         opcode
     }
 
-    pub fn decode(opcode: u16) {
-        let x = (opcode & 0x0F00) >> 8;
-        let y = (opcode & 0x00F0) >> 4;
+    pub fn decode(&mut self, opcode: u16) {
+        let x = ((opcode & 0x0F00) >> 8) as usize;
+        let y = ((opcode & 0x00F0) >> 4) as usize;
         let n = opcode & 0x000F;
-        let nn = opcode & 0x00FF;
+        let nn = (opcode & 0x00FF) as u8;
         let nnn = opcode & 0x0FFF;
 
         let nib_1 = (opcode & 0xF000) >> 12;
@@ -41,13 +41,22 @@ impl Chip8 {
         let nib_3 = (opcode & 0x00F0) >> 4;
         let nib_4 = opcode & 0x000F;
 
-        match nib_1, nib_2, nib_3, nib_4 {
+        match (nib_1, nib_2, nib_3, nib_4) {
             (0x0, 0x0, 0xE, 0x0) => {}
             (0x1, _, _, _) => self.pc = nnn,
             (0x6, _, _, _) => self.vr[x] = nn,
             (0x7, _, _, _) => self.vr[x] = self.vr[x].wrapping_add(nn),
             (0xA, _, _, _) => self.ir = nnn,
-            (0xD, _, _, _) => {}
+            (0xD, _, _, _) => {
+                // let mut sprite = Vec::<Vec<u8>>::new();
+                // for h in 0..n {
+                //     let mut row = Vec::<u8>::new();
+                //     let mut byte = 
+                // }
+                let sprite = &self.memory[self.ir as usize .. (self.ir + n) as usize];
+                self.display.draw(sprite, self.vr[x], self.vr[y]);
+            }
+            _ => todo!()
         }
     }
 
